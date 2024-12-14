a8c5e64bb968a1dac1dfd9ff6602a9b42b451524
taaag51 <inat0510@gmail.com>
2024-12-14 17:36:50 +0900

    add: usecaseの実装

--- go-rest-api/go.mod
+++ go-rest-api/go.mod
@@ -3,7 +3,9 @@ module go-rest-api
 go 1.23.4
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.1
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.17.0
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
 )
@@ -15,7 +17,6 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.10.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 )
--- go-rest-api/usecase/user_usecase.go
+++ go-rest-api/usecase/user_usecase.go
@@ -3,7 +3,10 @@ package usecase
 import (
 	"go-rest-api/model"
 	"go-rest-api/repository"
+	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -37,4 +40,24 @@ func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
 		Email: newUser.Email,
 	}
 	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user model.User) (string, error) {
+	storedUser := model.User{}
+	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
+		return "", err
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(user.Password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": storedUser.ID,
+		"exp":     time.Now().Add(time.Hour * 12).Unix(),
+	})
+	tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
 }
