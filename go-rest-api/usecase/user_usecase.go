fdf38b2073cdfb1ae7c80d71bbc7c8328aceba9b
taaag51 <inat0510@gmail.com>
2024-12-14 16:56:29 +0900

    add: user_usecaseにSignUpメソッドを追加

---
+++ go-rest-api/usecase/user_usecase.go
@@ -0,0 +1,40 @@
+package usecase
+
+import (
+	"go-rest-api/model"
+	"go-rest-api/repository"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type IUserUsecase interface {
+	SignUp(user model.User) (model.UserResponse, error)
+	Login(user model.User) (string, error)
+}
+
+type userUsecase struct {
+	ur repository.IUserRepository
+}
+
+func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
+	return &userUsecase{ur}
+}
+
+func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	if err != nil {
+		return model.UserResponse{}, err
+	}
+	newUser := model.User{
+		Email:    user.Email,
+		Password: string(hash),
+	}
+	if err := uu.ur.CreateUser(&newUser); err != nil {
+		return model.UserResponse{}, err
+	}
+	resUser := model.UserResponse{
+		ID:    newUser.ID,
+		Email: newUser.Email,
+	}
+	return resUser, nil
+}
