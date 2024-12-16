c4dbfc9f2045243f81512730d392ab8e83260764
taaag51 <inat0510@gmail.com>
2024-12-16 09:15:25 +0900

    add: ITaskRepository interface

---
+++ go-rest-api/repository/task_repositoty.go
@@ -0,0 +1,11 @@
+package repository
+
+import "go-rest-api/model"
+
+type ITaskRepository interface {
+	GetAllTasks(tasks *[]model.Task, userId uint) error
+	GetTaskById(task *model.Task, userId uint, taskId uint) error
+	CreateTask(task *model.Task) error
+	UpdateTask(task *model.Task, userId uint, taskId uint) error
+	DeleteTask(userId uint, taskId uint) error
+}
