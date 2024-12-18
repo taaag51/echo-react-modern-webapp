445d77abfc4593da01cd91e74df765f48d261c07
taaag51 <inat0510@gmail.com>
2024-12-18 23:43:57 +0900

    add: task_usecaseのコンストラクタ作成まで

---
+++ go-rest-api/usecase/task_usecase.go
@@ -0,0 +1,22 @@
+package usecase
+
+import (
+	"go-rest-api/model"
+	"go-rest-api/repository"
+)
+
+type ITaskUsecase interface {
+	GetAllTasks(userId uint) ([]model.TaskResponse, error)
+	GetTaskById(userId uint, taskId uint) (model.TaskResponse, error)
+	CreateTask(task model.Task) (model.TaskResponse, error)
+	UpdateTask(task model.Task, userId uint, taskId uint) (model.TaskResponse, error)
+	DeleteTask(userId uint, taskId uint) error
+}
+
+type taskUsecase struct {
+	tr repository.ITaskRepository
+}
+
+func NewTaskUsecase(tr repository.ITaskRepository) ITaskUsecase {
+	return &taskUsecase{tr}
+}
