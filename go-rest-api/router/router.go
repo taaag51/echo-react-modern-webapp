17cb2cbf3a0ce3ac7f86ee31d36649f5419093ee
taaag51 <inat0510@gmail.com>
2025-01-14 08:57:16 +0900

    add: CsrfTokenメソッドをユーザーコントローラーに追加し、CSRFトークンを返すAPIエンドポイントを実装

--- go-rest-api/router/router.go
+++ go-rest-api/router/router.go
@@ -22,7 +22,9 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
+		// CookieSameSite: http.SameSiteNoneMode,
 		CookieSameSite: http.SameSiteDefaultMode,
+		// CookieMaxAge: 60,
 	}))
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
