4fe91557a50fa680a4898ae9292f0c722cf56786
taaag51 <inat0510@gmail.com>
2025-01-15 08:45:26 +0900

    fix: セキュアなクッキー設定を有効化し、SameSiteポリシーを修正

--- go-rest-api/router/router.go
+++ go-rest-api/router/router.go
@@ -22,8 +22,8 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
-		// CookieSameSite: http.SameSiteNoneMode,
-		CookieSameSite: http.SameSiteDefaultMode,
+		CookieSameSite: http.SameSiteNoneMode,
+		// CookieSameSite: http.SameSiteDefaultMode,
 		// CookieMaxAge: 60,
 	}))
 	e.POST("/signup", uc.SignUp)
