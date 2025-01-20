6f343a4defb10f9e6a7da0f1a60eff03fef44ca0
taaag51 <inat0510@gmail.com>
2025-01-21 08:53:18 +0900

    chore:Authを 途中まで作成

--- react-todo/src/components/Auth.tsx
+++ react-todo/src/components/Auth.tsx
@@ -1,3 +1,33 @@
+import { CheckBadgeIcon, ArrowPathIcon } from '@heroicons/react/24/solid'
+import { useState, FormEvent } from 'react'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+
 export const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [pw, setPw] = useState('')
+  const [isLogin, setIsLogin] = useState(true)
+  const { loginMutation, registerMutation } = useMutateAuth()
+
+  const submitAuthHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (isLogin) {
+      loginMutation.mutate({
+        email: email,
+        password: pw,
+      })
+    } else {
+      await registerMutation
+        .mutateAsync({
+          email: email,
+          password: pw,
+        })
+        .then(() =>
+          loginMutation.mutate({
+            email: email,
+            password: pw,
+          })
+        )
+    }
+  }
   ;<div>Auth</div>
 }
--- react-todo/src/hooks/useMutateAuth.ts
+++ react-todo/src/hooks/useMutateAuth.ts
@@ -1,17 +1,17 @@
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useMutation } from '@tanstack/react-query'
-import useStore from '../store'
+import { useStore } from '../store'
 import { Credential } from '../types'
-import { useError } from '../hooks/useError'
+import { userError } from '../hooks/useError'
 
 export const useMutateAuth = () => {
   const navigate = useNavigate()
-  const resetEditedTask = useStore((state) => state.resetEditedTask)
-  const { switchErrorHandling } = useError()
+  const resetEditedTask = useStore((state) => state.resetEditedTask])
+  const { switchErrorHandling } = userError()
   const loginMutation = useMutation(
     async (user: Credential) =>
-      await axios.post(`${process.env.REACT_APP_API_URL}/login`, user),
+      await axios.post(`${process.env.REACT_APP_API_URL}/login`,user),
     {
       onSuccess: () => {
         navigate('/todo')
@@ -26,8 +26,7 @@ export const useMutateAuth = () => {
     }
   )
   const registerMutation = useMutation(
-    async (user: Credential) =>
-      await axios.post(`${process.env.REACT_APP_API_URL}/signup`, user),
+    async (user: Credential) => axios.post(`${process.env.REACT_APP_API_URL}/signup`, user),
     {
       onError: (err: any) => {
         if (err.response.data.message) {
@@ -39,7 +38,7 @@ export const useMutateAuth = () => {
     }
   )
   const logoutMutation = useMutation(
-    async () => await axios.post(`${process.env.REACT_APP_API_URL}/logout`),
+    async () => axios.post(`${process.env.REACT_APP_API_URL}/logout`),
     {
       onSuccess: () => {
         resetEditedTask()
@@ -54,5 +53,5 @@ export const useMutateAuth = () => {
       },
     }
   )
-  return { loginMutation, registerMutation, logoutMutation }
+  return { loginMutation, registerMutation, logoutMutation}
 }
